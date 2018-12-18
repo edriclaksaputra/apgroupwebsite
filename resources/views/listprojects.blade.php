a32254eb9d7aa477bf04b2efcc2a0ecc0c9c6742
Edric Laksa Putra <Edric Laksa Putra>
2018-12-18 12:59:17 +0700

    Deploy 1 time

--- resources/views/listprojects.blade.php
+++ resources/views/listprojects.blade.php
@@ -1,4 +1,61 @@
-@include('layouts.header')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" href="style/images/favicon.png">
+<title>a+pgrp architecture and master planning</title>
+<!-- Bootstrap core CSS -->
+<link href="style/css/bootstrap.min.css" rel="stylesheet">
+<link href="style/css/plugins.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<link href="style/css/color/blue.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<link href="style/type/icons.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body class="onepage">
+<div id="preloader"><div class="textload">Loading</div><div id="status"><div class="spinner"></div></div></div>
+<main class="body-wrapper">
+  <div class="navbar">
+    <div class="navbar-header">
+      <div class="basic-wrapper" style="margin-left: 0%">
+        <div class="navbar-brand"> <a href="/"><img src="#" srcset="style/images/logoapgrp.png 1x, style/images/logoapgrp.png 2x" class="logo-light" alt="" /><img src="#" srcset="style/images/logoapgrp.png 1x, style/images/logoapgrp.png 2x" class="logo-dark" alt="" /></a> </div>
+        <a class="btn responsive-menu" data-toggle="collapse" data-target=".navbar-collapse"><i></i></a> </div>
+      <!-- /.basic-wrapper --> 
+    </div>
+    <!-- /.navbar-header -->
+    <nav class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="/#home" title="home">Home</a></li>
+        <li><a href="/#about" title="about">About</a></li>
+        <li><a href="/#portfolio" title="portofolio">Projects</a></li>
+        <li><a href="/#locations" title="locations">Locations</a></li>
+        <li><a href="/#clients" title="jobs">Clients</a></li>
+      </ul>
+      <!-- /.navbar-nav --> 
+    </nav>
+    <!-- /.navbar-collapse -->
+    <div class="social-wrapper">
+      <ul class="social naked" style="margin-right: 10%">
+        <li><a href="#"><i class="icon-s-facebook"></i></a></li>
+        <li><a href="#"><i class="icon-s-linkedin"></i></a></li>
+        <li><a href="#"><i class="icon-s-instagram"></i></a></li>
+      </ul>
+      <!-- /.social --> 
+    </div>
+    <!-- /.social-wrapper --> 
+  </div>
+  <!-- /.navbar -->
+
   <div class="post-parallax parallax inverse-wrapper parallax2" style="background-image: url(style/images/art/parallax2.jpg);">
     <div class="container inner text-center">
       <div class="headline text-right">
